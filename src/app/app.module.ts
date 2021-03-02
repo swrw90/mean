b9cb5a4f007374f65e1cd432ef549576b8eb0cb7
Seth Watson <seth.watson1@gmail.com>
2021-03-02 01:17:50 -0800

    added MatButtonModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -18,7 +20,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
